ccf637e85099f4465d181ce03c3e2ceca71803b7
BBK-PiJ-2014-30 <devonmullings@yahoo.co.uk>
2014-12-17 17:21:31 +0000

    power

--- Power.java
+++ Power.java
@@ -4,27 +4,39 @@ import java.util.Scanner;
  * Created by devon on 17/12/2014.
  */
 public class Power {
+    static int result;
     public static void main(String[] args) {
 
         Scanner user = new Scanner(System.in);
-        int expon = user.nextInt();
+
         int bas = user.nextInt();
-        int result = power(bas, expon);
-        System.out.println( result);
+        int expon = user.nextInt();
+
+        System.out.println( power(bas, expon));
 
 
     }
 
-    static int power(int a, int b) {
+    static int power(int a,int b) {
+        result = a;
 
-        int result = a * a;
 
-
-        if (b == 0) {
+        if ( b==1){
             return result;
-        }
-        
-        else { return 0;
+
+
+    }
+        else{
+            result += result*a;
+            System.out.println( " B: "+ b);
+            power(a,b-1);
+            System.out.println(" Result :" +  result);
+            System.out.println( b);
+            return 2;}
+
+
+
+
     }
 }
-}
+

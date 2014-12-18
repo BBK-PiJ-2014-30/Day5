73de3025f66cf6f4b4b5d5f50417567b2bcfe236
BBK-PiJ-2014-30 <devonmullings@yahoo.co.uk>
2014-12-18 02:20:25 +0000

    Power: Recursive Method
    further modified. Program is a recursive method that calculates the
    power of a number.

--- Power.java
+++ Power.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
  * Created by devon on 17/12/2014.
  */
 public class Power {
-static int base;
+
     public static void main(String[] args) {
 
         Scanner user = new Scanner(System.in);
 
         int bas = user.nextInt();
-        base =bas;
+
         int expon = user.nextInt();
 
         int result = power(bas, expon);
@@ -30,9 +30,10 @@ static int base;
 
     }
         else {
-            a = a*base;
 
-          return power(a, b-1);}
+
+            --b;
+            return a * power( a, b);}
 
 
 
